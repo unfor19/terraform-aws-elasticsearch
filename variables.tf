4f351deb0b1f23cc119765c207199e5d89da6858
Meir Gabay <meir@prodops.io>
2020-08-05 17:38:20 +0300

    add variable

--- variables.tf
+++ variables.tf
@@ -275,6 +275,12 @@ variable "create_iam_service_linked_role" {
   description = "Whether to create `AWSServiceRoleForAmazonElasticsearchService` service-linked role. Set it to `false` if you already have an ElasticSearch cluster created in the AWS account and AWSServiceRoleForAmazonElasticsearchService already exists. See https://github.com/terraform-providers/terraform-provider-aws/issues/5218 for more info"
 }
 
+variable "iam_service_linked_role_custom_suffix" {
+  type        = string
+  default     = ""
+  description = "Add a custom suffix to avoid the error 'Service role name has been taken in this account'"
+}
+
 variable "node_to_node_encryption_enabled" {
   type        = bool
   default     = false
